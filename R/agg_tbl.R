0b9eb04c16447ed93e1ddb2d65e544e36fbbf409
Triamus <masteraxel@gmx.de>
2017-01-16 00:27:49 +0100

    adding some documentation, metadata and first function. agg_tbl call leads to setNames error

---
+++ R/agg_tbl.R
@@ -0,0 +1,29 @@
+#' Aggregates a data.table
+#' @param dt A data.table object
+#' @param metric A vector of numeric variable(s) to aggregate
+#' @param metric_name A vector of variable name(s) for the numeric variable(s) to aggregate. Defaults to metric.
+#' @param dimension A vector of dimension variable(s) to aggregate by
+#' @param dimension_name A vector of dimension variable name(s) for the dimension variable(s). Defaults to dimension.
+#' @param agg_fn An aggregation function
+#' @param na.rm logical. Should missing values (including NaN) be removed?
+#' @return Returns a data.table
+#' @export
+#' @examples
+#' \dontrun{
+#' to follow
+#' }
+
+agg_tbl <-
+  function(dt, metric, metric_name,
+           dimension, dimension_name,
+           agg_fn, na.rm = TRUE) {
+
+    e <- function(x) getFunction(agg_fn)(x, na.rm = na.rm)
+
+    temp <- dt[, setNames(lapply(.SD, e),
+                          metric_name),
+               keyby = dimension, .SDcols = metric]
+    temp[]
+  }
+
+
